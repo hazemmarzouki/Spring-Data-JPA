84fbf346c9f3c1485f85ec289b2baf5c05e36746
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-12-21 00:05:07 +0100

    initialize SpringBoot Project

---
+++ src/test/java/com/Hazem/Spring/Data/Jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hazem.Spring.Data.Jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
